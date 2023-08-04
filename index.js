0aac432252003dcda0544773e2db596a7906301c
juliabykovaa <97822247+juliabykovaa@users.noreply.github.com>
2023-08-04 17:46:03 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+import contactService from "./db/contacts.js";
+import { program } from "commander";
+program
+  .option("-a, --action <type>")
+  .option("-i, --id <type>")
+  .option("-n, --name <type>")
+  .option("-e, --email <type>")
+  .option("-p, --phone <type>");
+
+program.parse();
+const options = program.opts();
+
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      const allContacts = await contactService.listContacts();
+      return console.log("all contacts", allContacts);
+
+    case "get":
+      const oneContact = await contactService.getContactById(id);
+      return console.log("one contact", oneContact);
+
+    case "add":
+      const newContact = await contactService.addContact(name, email, phone);
+      return console.log("add contact", newContact);
+
+    case "remove":
+      const removedContact = await contactService.removeContact(id);
+      return console.log("remove contact", removedContact);
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+invokeAction(options);
+
+// invokeAction({action: 'list'})
+// invokeAction({ action: "get", id: "qdggE76Jtbfd9eWJHrssH" });
+
+// invokeAction({ action: 'add', name: "Nick", email: 'aaa@ff.com', phone: "2345678" })
+// invokeAction({ action: "remove", id: "AeHIrLTr6JkxGE6SN-0Rw" });
